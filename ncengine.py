ec9d6e489e6ac1b614f6c1fc864485c3a8b92176
Poul Kalff <poulkalff@gmail.com>
2018-07-22 17:04:22 +0200

    work on encoding

--- ncengine.py
+++ ncengine.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import re
 import sys
 import curses
 import locale
@@ -13,57 +14,138 @@ code = locale.getpreferredencoding()
 
 # --- Classes -------------------------------------------------------------------------------------
 
+class FlipSwitch():
+    # (NEW) Represents a switch with on and off-state
+
+    def __init__(self, Ind):
+        self._value = bool(Ind)
+
+    def flip(self):
+        if self._value == True:
+            self._value = False
+        else:
+            self._value = True
+
+    def get(self):
+        return self._value
+
+    def getString(self):
+        return str(self._value)
+
+
+class RangeIterator():
+    # (NEW) Represents a range of INTs from 0 -> X
+
+    def __init__(self, Ind, loop=True):
+        self.current = 0
+        self.max = Ind
+        self.loop = loop
+
+    def inc(self, count=1):
+        self.current += count
+        self._test()
+
+    def dec(self, count=1):
+        self.current -= count
+        self._test()
+
+    def _test(self):
+        if self.loop:
+            if self.current > self.max:
+                self.current -= self.max + 1
+            elif self.current < 0:
+                self.current += self.max + 1
+        elif not self.loop:
+            if self.current >= self.max:
+                self.current = self.max
+            elif self.current < 0:
+                self.current = 0
+
+    def get(self):
+        return self.current
+
+class renderLine:
+    """ A renderLine object """
+
+    def __init__(self, xPos, yPos, text, color):
+        self.x = xPos
+        self.y = yPos
+        self.text = text
+        self.maxX = x + len(re.sub('[^\w\s]', '', text))
+        self.color = color
+
+
+class Menu:
+    """ A menu object """
+
+    def __init__(self, xPos, yPos, items, pointer):
+        self.x = xPos
+        self.y = yPos
+        self.menuItems = items
+        self.highlighted =  None
+        self.prevPointer = pointer
+
+    def getCoords(self):
+        return (self.x, self.y)
+
+
+
 class NCEngine:
 	""" Presents the screen of a program """
 
 	frame = []
-	frameMin = [10,10]
 	menus = []
+	color = { 'white': 0, 'red': 1, 'green' : 2, 'orange' : 3, 'blue' : 4, 'purple' : 5, 'cyan' : 6, 'lightgrey' : 7}	# basic colors
+
 
 	def __init__(self):
 		self.screen = curses.initscr()
 		self.screen.border(0)
 		self.screen.keypad(1)
-		self.updatePosition()
+		self.getSize()
 		curses.noecho()
-		curses.start_color()
 		curses.curs_set(0)
-		curses.init_pair(1, curses.COLOR_RED, curses.COLOR_WHITE)			# more generic... have 2 standard, and other must be custom?
-		curses.init_pair(2, curses.COLOR_CYAN, curses.COLOR_BLACK)
-		curses.init_pair(3, curses.COLOR_BLUE, curses.COLOR_BLACK)
-		curses.init_pair(4, curses.COLOR_GREEN, curses.COLOR_BLACK)
-		curses.init_pair(5, curses.COLOR_YELLOW, curses.COLOR_BLACK)
-		curses.init_pair(6, curses.COLOR_RED, curses.COLOR_BLACK)
-		curses.init_pair(7, curses.COLOR_WHITE, curses.COLOR_BLUE)
+		# init colors
+		curses.start_color()
+		curses.use_default_colors()
+		for i in range(0, curses.COLORS):
+			curses.init_pair(i, i, -1)
+		curses.init_pair(300, curses.COLOR_RED, curses.COLOR_WHITE)			# init 256 colors + 1 special
 
 
-	def updatePosition(self):
+	def showColors(self):
+		""" Show all colors available with their numbers (helper function) """
+		sys.exit('notImplemented')
+
+
+	def getSize(self):
+		""" Update height/width/center """
 		self.height, self.width = self.screen.getmaxyx()
-		self.center = (self.width - 1) / 2
+		self.hcenter = (self.width - 1) / 2
+		self.vcenter = (self.height - 1) / 2
 
 
-	def createMenu(self, posX, posY, items):
+	def createMenu(self, posX, posY, items, color, frame = True):
 		""" Creates the data used for painting a menu """
 		screenData = []
-		self.updatePosition()
+		self.getSize()
 		maxLength = len(max(items, key=lambda coll: len(coll)))
 		maxHeight = len(items)
-		screenData.append([posX, posY, '╭─' + ('─' * maxLength) + '─╮', 5])					# Top
+		# frame
+		if frame:
+			for nr, item in enumerate(items):
+				screenData.append([posX, posY + nr + 1, '│ ' + (maxLength * ' ')  + ' │', color])
+			screenData.append([posX, posY, '╭─' + ('─' * maxLength) + '─╮', color])						# Top
+			screenData.append([posX, posY + maxHeight + 1, '└─' + ('─' * maxLength) + '─╯', color])		# Bottom
+		# text
 		for nr, item in enumerate(items):
-			missingSpace = maxLength - len(item)
-			screenData.append([posX, posY + nr + 1, '│ ' + item + (missingSpace * ' ')  + ' │', 5])
-		screenData.append([posX, posY + maxHeight + 1, '└─' + ('─' * maxLength) + '─╯', 5])		# Bottom
+			screenData.append([posX + 2, posY + nr + 1, item, color])
 		self.menus.append(screenData)
-
-
-	def lastMenuSpecs(self):
-		""" Get specifications of the last menu """
-		pass	# Use this to build next menu
 
 
 	def render(self):
 		""" handles resize and displays the data in "data" """
-		self.updatePosition()
+		self.getSize()
 		self.screen.clear()
 		# check if resized
 		if curses.is_term_resized(self.height, self.width):
@@ -74,17 +156,16 @@ class NCEngine:
 			for line in menu:
 				dispData.append(line)
 		# paint window
-		if self.height > self.frameMin[0] and self.width > self.frameMin[1]:    # Match text when populated
+		if self.height > 5 and self.width > 5:    # Match text when populated
 			for x, y, text, color in dispData:
-				totalLength = x + len(text)
-				if totalLength > self.width:
-					text = text[:self.width - 6] + ' >>'
+				if x + len(re.sub('[^\w\s]', '', text)) > self.width:
+					text = text[:self.width - 5] + ' ⇨'
 				if y < self.height:
 					self.screen.addstr(y, x, str(text), curses.color_pair(color))
 				else:
-					self.screen.addstr(self.height - 1, 2, '^ ' * (self.width / 2 - 2), curses.color_pair(color))
+					self.screen.addstr(self.height - 1, x, '⇩' * (len(text) / 3), curses.color_pair(color))
 		elif self.height > 1 and self.width > 5:
-			self.screen.addstr(0, 0, "Window not displayed", curses.color_pair(1))
+			self.screen.addstr(0, 0, "Window not displayed", curses.color_pair(300))
 		self.screen.refresh()
 
 
@@ -106,5 +187,5 @@ class NCEngine:
 
 
 # --- TODO ---------------------------------------------------------------------------------------
-# -
+# - BUG : Noget galt med '>>', det vises på menuer...?
 
--- test.py
+++ test.py
@@ -21,17 +21,27 @@ running = True
 obj = NCEngine()
 
 # add dat to frame/display
-obj.frame.append([2, 2,  'Are you sure thatINAL file "' + '1' + '" with its backup?' , 5])
-obj.frame.append([2, 3,  'This is a potentially very dangerous operats a potentially very dangerous operats a potentially very dangerous operats a potentially very dangerous operation!!!', 5])
-obj.frame.append([2, 4,  'Please type in "doit!" to complete the operation', 5])
-obj.frame.append([2, 5,  'Any other command cancels request', 5])
-obj.frame.append([2, 32, 'Any other command cancels request', 5])
+obj.frame.append([2, 2,  'Any other command cancels request', 0])
+obj.frame.append([2, 3,  'Any other command cancels request', 1])
+obj.frame.append([2, 4,  'Any other command cancels request', 2])
+obj.frame.append([2, 5,  'Any other command cancels request', 3])
+obj.frame.append([2, 6,  'Any other command cancels request', 4])
+obj.frame.append([2, 7,  'Any other command cancels request', 5])
+obj.frame.append([2, 8,  'Any other command cancels request', 6])
+obj.frame.append([2, 9,  'Any other command cancels request', 7])
+obj.frame.append([2, 10,  'Any other command cancels request', 8])
+obj.frame.append([2, 11,  'Any other command cancels request', 9])
+obj.frame.append([2, 12,  'Any other command cancels request', 10])
+obj.frame.append([2, 13,  'Any other command cancels request', 11])
+obj.frame.append([2, 14,  'Any other command cancels request', 12])
+obj.frame.append([2, 15,  'Any other command cancels request', 13])
+obj.frame.append([2, 16,  'Any other command cancels request', 14])
+obj.frame.append([2, 17,  'Any other command cancels request', 15])
+
 # add Menu
-obj.createMenu(20, 10, ["First", "Second", "Third", "Fourthflippingfourth"])
-obj.createMenu(20, 21, ['1', '2', '3'])
-obj.createMenu(25, 20, ['test123'])
-
-
+obj.createMenu(20, 10, ["First", "Second", "Third", "Fourthflippingfourth"], obj.color['red'], True)
+obj.createMenu(20, 21, ['1', '2', '3'], obj.color['green'])
+obj.createMenu(25, 20, ['test123'], obj.color['blue'])
 
 # loop and test keys
 while running:
