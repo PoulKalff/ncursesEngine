950e338b6ff198dd3750f5e9806c72e19b0cf80a
Poul Kalff <poulkalff@gmail.com>
2018-08-02 20:28:12 +0200

    added properties, lines, objects

--- ncengine.py
+++ ncengine.py
@@ -14,77 +14,84 @@ locale.setlocale(locale.LC_ALL, '')
 # --- Classes -------------------------------------------------------------------------------------
 
 class FlipSwitch():
-    # (NEW) Represents a switch with on and off-state
+	# (NEW) Represents a switch with on and off-state
 
-    def __init__(self, Ind):
-        self._value = bool(Ind)
+	def __init__(self, Ind):
+		self._value = bool(Ind)
 
-    def flip(self):
-        if self._value == True:
-            self._value = False
-        else:
-            self._value = True
+	def flip(self):
+		if self._value == True:
+			self._value = False
+		else:
+			self._value = True
 
-    def get(self):
-        return self._value
+	def get(self):
+		return self._value
 
-    def getString(self):
-        return str(self._value)
+	def getString(self):
+		return str(self._value)
 
 
 class RangeIterator():
-    # (NEW) Represents a range of INTs from 0 -> X
+	# (NEW) Represents a range of INTs from 0 -> X
 
-    def __init__(self, Ind, loop=True):
-        self.current = 0
-        self.max = Ind
-        self.loop = loop
+	def __init__(self, Ind, loop=True):
+		self.current = 0
+		self.max = Ind
+		self.loop = loop
 
-    def inc(self, count=1):
-        self.current += count
-        self._test()
+	def inc(self, count=1):
+		self.current += count
+		self._test()
 
-    def dec(self, count=1):
-        self.current -= count
-        self._test()
+	def dec(self, count=1):
+		self.current -= count
+		self._test()
 
-    def _test(self):
-        if self.loop:
-            if self.current > self.max:
-                self.current -= self.max + 1
-            elif self.current < 0:
-                self.current += self.max + 1
-        elif not self.loop:
-            if self.current >= self.max:
-                self.current = self.max
-            elif self.current < 0:
-                self.current = 0
+	def _test(self):
+		if self.loop:
+			if self.current > self.max:
+				self.current -= self.max + 1
+			elif self.current < 0:
+				self.current += self.max + 1
+		elif not self.loop:
+			if self.current >= self.max:
+				self.current = self.max
+			elif self.current < 0:
+				self.current = 0
 
-    def get(self):
-        return self.current
+	def get(self):
+		return self.current
 
 
 class Menu:
-    """ A menu object """
+	""" A menu object """
 
-    def __init__(self, xPos, yPos, items, pointer):
-        self.x = xPos
-        self.y = yPos
-        self.menuItems = items
-        self.highlighted =  None
-        self.prevPointer = pointer
+	def __init__(self, xPos, yPos, items, color, mWidth, frame):
+		self.x = xPos
+		self.y = yPos
+		self.frame = frame
+		self.color = color
+		self.items = items
+		self.maxWidth = mWidth
+		self.highlighted =  None
+		self.pointer = RangeIterator(len(items) - 1, False)
 
-    def getCoords(self):
-        return (self.x, self.y)
+	def getCoords(self):
+		return (self.x, self.y)
 
 
 
 class NCEngine:
 	""" Presents the screen of a program """
 
-	frame = []
-	menus = []
+	objects = []
 	color = { 'white': 0, 'red': 1, 'green' : 2, 'orange' : 3, 'blue' : 4, 'purple' : 5, 'cyan' : 6, 'lightgrey' : 7}	# basic colors
+	border = False
+	_borderColor = 0
+	status = 'Init'
+	lines = []
+	topMenus = []
 
 
 	def __init__(self):
@@ -111,26 +118,15 @@ class NCEngine:
 	def getSize(self):
 		""" Update height/width/center """
 		self.height, self.width = self.screen.getmaxyx()
-		self.hcenter = (self.width - 1) / 2
+		self.hcenter = int((self.width - 1) / 2)
 		self.vcenter = (self.height - 1) / 2
 
 
 	def createMenu(self, posX, posY, items, color, frame = True):
 		""" Creates the data used for painting a menu """
-		screenData = []
 		self.getSize()
 		maxLength = len(max(items, key=lambda coll: len(coll)))
-		maxHeight = len(items)
-		# frame
-		if frame:
-			for nr, item in enumerate(items):
-				screenData.append([posX, posY + nr + 1, '│ ' + (maxLength * ' ')  + ' │', color])
-			screenData.append([posX, posY, '╭─' + ('─' * maxLength) + '─╮', color])						# Top
-			screenData.append([posX, posY + maxHeight + 1, '└─' + ('─' * maxLength) + '─╯', color])		# Bottom
-		# text
-		for nr, item in enumerate(items):
-			screenData.append([posX + 2, posY + nr + 1, item, color])
-		self.menus.append(screenData)
+		self.objects.append(Menu(posX, posY, items, color, maxLength, frame))
 
 
 	def render(self):
@@ -140,23 +136,67 @@ class NCEngine:
 		# check if resized
 		if curses.is_term_resized(self.height, self.width):
 			curses.resizeterm(self.height, self.width)
-		# collect data
-		dispData = self.frame
-		for menu in self.menus:
-			for line in menu:
-				dispData.append(line)
-		# paint window
-		if self.height > 5 and self.width > 5:    # Match text when populated
-			for x, y, text, color in dispData:
-				if x + len(re.sub('[^\w\s]', '', text)) > self.width:
-					text = text[:self.width - 5] + ' ⇨'
-				if y < self.height:
-					self.screen.addstr(y, x, text, curses.color_pair(color))
-				else:
-					self.screen.addstr(self.height - 1, x, '⇩' * len(re.sub('[^\w\s]', '', text)), curses.color_pair(color))
-		elif self.height > 1 and self.width > 5:
-			self.screen.addstr(0, 0, "Window not displayed", curses.color_pair(300))
+		# render border
+		if self.border:
+			self.drawBorder()
+		# render lines
+		self.drawLines()
+		# render status
+		self.screen.addstr(self.height - 1, 1, self.status , curses.color_pair(self.borderColor))
+		# render topMenus
+		for nr, m in enumerate(self.topMenus):
+			screenPartWidth = self.width / len(self.topMenus)
+			self.screen.addstr(1, int(nr * screenPartWidth + 2), m , curses.color_pair(self.borderColor))
+		# render each object
+		for menu in self.objects:
+			maxHeight = len(menu.items)
+			# frame
+			if menu.frame:
+				if menu.maxWidth + menu.x < self.width:
+					for nr, item in enumerate(menu.items):
+						self.screen.addstr(menu.y + nr + 1, menu.x, '│ ' + (menu.maxWidth * ' ')  + ' │', curses.color_pair(menu.color))
+				self.screen.addstr(menu.y, menu.x, '╭─' + ('─' * menu.maxWidth) + '─╮', curses.color_pair(menu.color))						# Top
+				self.screen.addstr(menu.y + maxHeight + 1, menu.x, '└─' + ('─' * menu.maxWidth) + '─╯', curses.color_pair(menu.color))		# Bottom
+			# text
+			for nr, item in enumerate(menu.items):
+				self.screen.addstr(menu.y + nr + 1, menu.x + 2, item, curses.color_pair(menu.color))
 		self.screen.refresh()
+
+
+	def drawLines(self):
+		""" Draw all lines added to object (except border) """
+		intersections = [[], []]
+		for l in self.lines:
+			if l[0] == 'v':
+				position = int((l[1] * self.width / 100) if type(l[1]) == float else l[1])
+				intersections[0].append(position)
+				for yPos in range(1, self.height - 2):
+					self.screen.addstr(yPos, position, '│', curses.color_pair(self.borderColor))
+				# endpoints
+				self.screen.addstr(0, position, '┬', curses.color_pair(self.borderColor))
+				self.screen.addstr(self.height - 2, position, '┴', curses.color_pair(self.borderColor))
+			elif l[0] == 'h':
+				position = int((l[1] * self.height / 100) - 1 if type(l[1]) == float else l[1])
+				intersections[1].append(position)
+				self.screen.addstr(position, 1, '─' * (self.width - 2), curses.color_pair(self.borderColor))
+				# endpoints
+				self.screen.addstr(position, 0, '├', curses.color_pair(self.borderColor))
+				self.screen.addstr(position, self.width - 1, '┤', curses.color_pair(self.borderColor))
+		# draw intersections
+		for x in intersections[1]:
+			for y in intersections[0]:
+				self.screen.addstr(x, y, '┼', curses.color_pair(self.borderColor))
+
+
+	def drawBorder(self):
+		""" Draw the staic border of the screen """
+		# horizontal lines
+		self.screen.addstr(0, 0, '╭' + '─' * (self.width - 2) + '╮', curses.color_pair(self.borderColor))						# Top
+		self.screen.addstr(self.height - 2, 0, '└' + '─' * (self.width - 2) + '╯', curses.color_pair(self.borderColor))			# Bottom
+		# vertical lines
+		for yPos in range(1, self.height - 2):
+			self.screen.addstr(yPos, 0, '│', curses.color_pair(self.borderColor))
+			self.screen.addstr(yPos, self.width - 1, '│', curses.color_pair(self.borderColor))
 
 
 	def getInput(self):
@@ -173,9 +213,23 @@ class NCEngine:
 		curses.endwin()
 
 
+# --- Setter Functions ----------------------------------------------------------------------------
+
+
+	@property
+	def borderColor(self):
+		return self._borderColor
+
+	@borderColor.setter
+	def borderColor(self, val):
+		self._borderColor = self.color[val.lower()] if type(val) == str else val
+
+
 
 
 
 # --- TODO ---------------------------------------------------------------------------------------
-# - BUG : Stadigt problemer med scroll/count af bytes.....
-
+# - BUG 	: Stadigt problemer med scroll/count af bytes.....
+# - FEATURE : Lav andre objecter end menu (textcollection, og ??), og lad dem arve fra et standard-object
+# - FEATURE : lave et MIN, så screen ikke renderes hvis objects ikke kan vaere paa skaermen
+# - FEATURE : Lave properties til alt, som kan indstilles
--- test.py
+++ test.py
@@ -21,27 +21,39 @@ running = True
 obj = NCEngine()
 
 # add dat to frame/display
-obj.frame.append([2, 2,  'Any other command cancels request', 0])
-obj.frame.append([2, 3,  'Any other command cancels request', 1])
-obj.frame.append([2, 4,  'Any other command cancels request', 2])
-obj.frame.append([2, 5,  'Any other command cancels request', 3])
-obj.frame.append([2, 6,  'Any other command cancels request', 4])
-obj.frame.append([2, 7,  'Any other command cancels request', 5])
-obj.frame.append([2, 8,  'Any other command cancels request', 6])
-obj.frame.append([2, 9,  'Any other command cancels request', 7])
-obj.frame.append([2, 10,  'Any other command cancels request', 8])
-obj.frame.append([2, 11,  'Any other command cancels request', 9])
-obj.frame.append([2, 12,  'Any other command cancels request', 10])
-obj.frame.append([2, 13,  'Any other command cancels request', 11])
-obj.frame.append([2, 14,  'Any other command cancels request', 12])
-obj.frame.append([2, 15,  'Any other command cancels request', 13])
-obj.frame.append([2, 16,  'Any other command cancels request', 14])
-#obj.frame.append([2, 37,  'Any other command cancels request', 15])
+# obj.objects.append([2, 2,  'Any other command cancels request', 0])
+# obj.objects.append([2, 3,  'Any other command cancels request', 1])
+# obj.objects.append([2, 4,  'Any other command cancels request', 2])
+# obj.objects.append([2, 5,  'Any other command cancels request', 3])
+# obj.objects.append([2, 6,  'Any other command cancels request', 4])
+# obj.objects.append([2, 7,  'Any other command cancels request', 5])
+# obj.objects.append([2, 8,  'Any other command cancels request', 6])
+# obj.objects.append([2, 9,  'Any other command cancels request', 7])
+# obj.objects.append([2, 10,  'Any other command cancels request', 8])
+# obj.objects.append([2, 11,  'Any other command cancels request', 9])
+# obj.objects.append([2, 12,  'Any other command cancels request', 10])
+# obj.objects.append([2, 13,  'Any other command cancels request', 11])
+# obj.objects.append([2, 14,  'Any other command cancels request', 12])
+# obj.objects.append([2, 15,  'Any other command cancels request', 13])
+# obj.objects.append([2, 16,  'Any other command cancels request', 14])
+#obj.objects.append([2, 37,  'Any other command cancels request', 15])
+
+obj.border = True
+obj.borderColor = 1
+obj.lines.append(['v', 50.0])
+obj.lines.append(['v', 30])
+obj.lines.append(['h', 2])
+obj.lines.append(['h', 21.0])
+
 
 # add Menu
 obj.createMenu(20, 10, ["First", "Second", "Third", "Fourthflippingfourth"], obj.color['red'], True)
-obj.createMenu(20, 21, ['1', '2', '3'], obj.color['green'])
+obj.createMenu(20, 21, ['1', '2', '3'], obj.color['green'], False)
 obj.createMenu(25, 20, ['test123'], obj.color['blue'])
+# add top menus
+obj.topMenus.append('Menu1')
+obj.topMenus.append('Menu2')
+
 
 # loop and test keys
 while running:
